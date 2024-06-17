b3035794c1e3bb8d03d52face0f2cfe2df4ca2cf
Yogesh Vashisth <writetokhair@gmail.com>
2024-06-17 12:34:28 +0530

    changed get to post in userTest route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,6 @@ const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(login);
-router.route("/usertest").post(userTest);
+router.route("/usertest").get(userTest);
 
 export default router;
