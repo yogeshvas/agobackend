aa98a27f74a1d8dd4ececd8046455aef19e48d30
Yogesh Vashisth <writetokhair@gmail.com>
2024-07-10 01:04:42 +0530

    bodyParserDone

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(bodyParser.json());
+app.use(bodyParser());
 
 // Routes
 app.use("/api/v1/users", userRoutes);
