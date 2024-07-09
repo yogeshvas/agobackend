65cd42b6a5e485c4d3646353916d924f464c9bf1
Yogesh Vashisth <writetokhair@gmail.com>
2024-07-10 01:07:20 +0530

    bodyParserFix

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 // Routes
 app.use("/api/v1/users", userRoutes);
